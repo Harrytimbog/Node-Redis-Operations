77a15971583b18361768f4186faae6b0374aa228
Timilehin Arigbede <Harriemannie@gmail.com>
2021-11-21 10:33:29 +0100

    How to store key-value pairs in Redis storage (strings)

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const client = redis.createClient();
 client.on("connect", function () {
   console.log("Connected!");
 });
+
+client.set("framework", "ReactJS", function (err, reply) {
+  console.log(reply); // OK
+});
+
+client.get("framework", function (err, reply) {
+  console.log(reply); // ReactJS
+});
